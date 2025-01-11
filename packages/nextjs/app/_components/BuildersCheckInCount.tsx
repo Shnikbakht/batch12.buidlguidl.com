a5e707dfd90aa98b0498174c77280b053ce5de02
Jason Suarez <suarezjason@icloud.com>
2025-01-11 07:59:51 +0100

    Refactor BuildersCheckInCount to use Scaffold Eth hook
    - Use useScaffoldReadContract to simplify the counter query to its simplest form.
    - Removed the ABI manual implementation and the usage of wagmi hook to interact with smart contract.

--- packages/nextjs/app/_components/BuildersCheckInCount.tsx
+++ packages/nextjs/app/_components/BuildersCheckInCount.tsx
@@ -2,58 +2,32 @@
 
 import React from "react";
 import { displayTxResult } from "../debug/_components/contract";
-import { useReadContract } from "wagmi";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
-import { useDeployedContractInfo, useTargetNetwork } from "~~/hooks/scaffold-eth";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const CONTRACT_NAME = "BatchRegistry";
-
-// Define the ABI for the BatchRegistry contract
-// The ABI is a JSON object that defines the functions and their parameters of a contract
-// Could be generated from the contract's ABI file or manually created
-// Check packages/nextjs/app/debug/_components/contract/ContractVariables.tsx for an example of how to use the ABI programmatically
-const BatchRegistryABI = {
-  checkedInCounter: {
-    name: "checkedInCounter",
-    type: "function",
-    stateMutability: "view",
-    inputs: [],
-    outputs: [{ type: "uint256" }],
-  },
-} as const;
+const FUNCTION_NAME = "checkedInCounter";
 
 function BuildersCheckInCount() {
-  // Get the deployed contract data
-  const { data: deployedContractData, isLoading: deployedContractLoading } = useDeployedContractInfo({
-    contractName: CONTRACT_NAME,
-  });
-
-  const { targetNetwork } = useTargetNetwork();
-
   // Get the result of the target function call from the deployed contract
   const {
-    data: result,
-    isFetching,
+    data: counter,
+    isLoading,
     error,
-  } = useReadContract({
-    address: deployedContractData?.address,
-    functionName: BatchRegistryABI.checkedInCounter.name,
-    abi: [BatchRegistryABI.checkedInCounter],
-    chainId: targetNetwork.id,
-    query: {
-      retry: false,
-    },
+  } = useScaffoldReadContract({
+    contractName: CONTRACT_NAME,
+    functionName: FUNCTION_NAME,
   });
 
   return (
     <div className="text-lg flex gap-2 justify-center items-center">
       <span className="font-bold">Checked in builders count:</span>
-      {deployedContractLoading || isFetching ? (
+      {isLoading || (!error && !counter) ? (
         <ArrowPathIcon className="size-5 animate-spin" />
       ) : error ? (
         <span className="text-error">Error!</span>
       ) : (
-        <span>{displayTxResult(result)}</span>
+        <span>{displayTxResult(counter)}</span>
       )}
     </div>
   );
