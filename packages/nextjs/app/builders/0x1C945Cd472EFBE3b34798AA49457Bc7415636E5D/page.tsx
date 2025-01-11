79b3904910ea1c08fa50d2bced1efd4d111909dc
obah <olusojiobah@gmail.com>
2025-01-11 20:43:03 +0100

    chore: optimized ui and content

--- packages/nextjs/app/builders/0x1C945Cd472EFBE3b34798AA49457Bc7415636E5D/page.tsx
+++ packages/nextjs/app/builders/0x1C945Cd472EFBE3b34798AA49457Bc7415636E5D/page.tsx
@@ -5,6 +5,12 @@ import { Address } from "~~/components/scaffold-eth";
 const obahProfile: NextPage = () => {
   const address = "0x1C945Cd472EFBE3b34798AA49457Bc7415636E5D";
 
+  const socialLinks = [
+    { name: "github", link: "https://github.com/obah" },
+    { name: "x", link: "https://x.com/oba_ddev" },
+    { name: "medium", link: "https://medium.com/@olusojiobah/" },
+  ] as const;
+
   return (
     <main className="mx-auto w-[90%] md:w-4/5 lg:w-[90%] xl:max-w-7xl">
       <section className="flex flex-col-reverse items-center gap-6 px-5 py-3 mt-10 bg-base-100 shadow-xl rounded-lg md:py-14 md:px-10 md:mt-20 lg:grid lg:grid-cols-2 xl:gap-10">
@@ -14,48 +20,38 @@ const obahProfile: NextPage = () => {
 
             <Address address={address} />
 
-            <p className="text-xl font-bold">Web3 Security Researcher</p>
+            <p className="text-xl font-bold">Smart Contract Developer</p>
           </div>
 
           <div>
             <h2 className="mb-6 text-2xl font-extrabold">About Me</h2>
 
             <p>
-              Hi there, I am Obaloluwa, a software developer passionate about blockchain and securing it. I have a
-              background in frontend development using Javascript, React and TailwindCSS.
+              Hi there, I am Obaloluwa, a software developer with a background in frontend development using{" "}
+              <b>Javascript, React and TailwindCSS</b>.
             </p>
+
             <p>
-              I am also proficient in Solidity and I am currently learning Rust and ZK while honing my smart contract
-              auditing and writing skills. Let us connect and build together 🙂
+              Passionate about <b>blockchain, blockchain security and zero knowledge</b>. I am proficient in{" "}
+              <b>Solidity</b> and currently learning <b>Rust</b> and ZK while honing my smart contract auditing and
+              writing skills.
             </p>
+
+            <p>Let us connect and build together 🙂</p>
           </div>
 
           <div className="flex gap-4 items-center">
-            <a
-              href="https://github.com/obah"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-primary btn-sm font-normal gap-1"
-            >
-              GitHub
-            </a>
-
-            <a
-              href="https://x.com/oba_ddev"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-primary btn-sm font-normal gap-1"
-            >
-              X
-            </a>
-            <a
-              href="https://medium.com/@olusojiobah/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-primary btn-sm font-normal gap-1"
-            >
-              Medium
-            </a>
+            {socialLinks.map(item => (
+              <a
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-primary capitalize w-24 btn-sm font-normal gap-1"
+                key={item.link}
+              >
+                {item.name}
+              </a>
+            ))}
           </div>
         </div>
 
